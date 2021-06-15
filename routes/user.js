a8e54e4b0be524e3c5a6b422035bd37c9e12a5ea
Andrea Clark <andrea_clark@glic.com>
2021-06-15 15:53:12 -0500

    migrated and seeded into my dev

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const ctrl = require('../controllers');
 
 router.post('/signup', ctrl.user.signup);
 router.post('/login', ctrl.user.login);
-router.get('/profile', ctrl.user.getProfile);
-router.put('/profile', ctrl.user.editProfile);
+router.get('/profile/:id', ctrl.user.getProfile);
+router.put('/profile;id', ctrl.user.editProfile);
 
 
 
